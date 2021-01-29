8e5a4a4ffc5e060d8648bd01094cff97296f176f
Dallin Gilbert <djgilb24@byui.edu>
2021-01-28 17:46:17 -0700

    correcting makefile and first test

--- deque/assignment04.cpp
+++ deque/assignment04.cpp
@@ -36,9 +36,9 @@ void testErrors();
 // I suggest first commenting out all of these tests, then try to use only
 // TEST1.  Then, when TEST1 works, try TEST2 and so on.
 #define TEST1   // for testSimple()
-#define TEST2   // for testPushPopFront()
-#define TEST3   // for testWrapping()
-#define TEST4   // for testErrors()
+//#define TEST2   // for testPushPopFront()
+//#define TEST3   // for testWrapping()
+//#define TEST4   // for testErrors()
 
 /**********************************************************************
  * MAIN
--- deque/deque.h
+++ deque/deque.h
@@ -57,7 +57,7 @@ namespace custom
         int iFrontNormalized() const;
         int iBackNormalized() const;
         int iNormalized(int index) const;
-        void display() const;
+        //void display() const;
 
     public:
         // default constructor and non-default constructors
@@ -138,7 +138,7 @@ namespace custom
 
         setCapacity(newCap);
         setFront(size());
-        setBack(0);
+        setBack(-1);
     };
 
     /********************************************
@@ -242,7 +242,7 @@ namespace custom
         if (!empty())
             setBack(getBack() - 1);
         else
-            return "Error!";
+            cout << "Error!";
     };
 
     /********************************************
@@ -256,7 +256,7 @@ namespace custom
         if (!empty())
             setFront(getFront() + 1);
         else
-            return "Error!";
+            cout << "Error!";
     };
 
     /*******************************************
@@ -296,7 +296,7 @@ namespace custom
         this->data = NULL;
         setFront(0);
         setCapacity(0);
-        setBack(0);
+        setBack(-1);
 
         *this = rhs;
     }
@@ -333,7 +333,7 @@ namespace custom
 
         // copy over the stuff
         setCapacity(numCapacity);
-        setBack(0);
+        setBack(-1);
         setFront(0);
     }
 
@@ -394,89 +394,3 @@ namespace custom
     }
 };
 #endif // DEQUE_H
-
-// this code should be copy-pasted into deque.h immediately after
-// the class definition. Also, put the following inside the class
-// definition itself:
-//     void display() const;
-// Please remove this comment when you have done that.
-//
-// To "turn this on", you need to add -DDEBUG to the makefile
-// in all the g++ statements. Please make sure you remove
-// these when you submit your assignment.
-//
-// Finally, to display the contents of the deque at a givem
-// point in the program (hopefully where the bugs are), then
-// you should add:
-//     d.display();
-
-#ifdef DEBUG
-
-using std::cerr;
-/****************************************************
- * DISPLAY DEQUE
- * display the contents of a deque for debug purposes
- ****************************************************/
-template <class T>
-void deque <T> :: display() const
-{
-   // display the header info
-   cerr << "\ndeque<T>::display()\n";
-   cerr << "\tnumCapacity = " << numCapacity << "\n";
-
-   // display the contents of the array
-   cerr << "\tdata = ";
-   if (numCapacity == 0)
-      cerr << "NULL";
-   else
-   {
-      cerr << "{ ";
-      for (int i = 0; i < numCapacity; i++)
-      {
-         if (i != 0)
-            cerr << ", ";
-
-         // not wrapped
-         //      0   1   2   3   4   5   6
-         //    +---+---+---+---+---+---+---+
-         //    |   |   | A | B | C |   |   |
-         //    +---+---+---+---+---+---+---+
-         // iFront = 9     iFrontNormalize() = 2
-         // iBack  = 11    iBackNormalize()  = 4
-         if (iFrontNormalize() <= iBackNormalize() &&  // not wrapped
-             iFrontNormalize() <= i &&
-             i <= iBackNormalize())                        // in range
-            cerr << data[i];
-
-         // wrapped
-         //      0   1   2   3   4   5   6
-         //    +---+---+---+---+---+---+---+
-         //    | B | C |   |   |   |   | A |
-         //    +---+---+---+---+---+---+---+
-         // iFront = -8    iFrontNormalize() = 6
-         // iBack  = -6    iBackNormalize()  = 1
-         else if (iFrontNormalize() > iBackNormalize() && // wrapped
-                  size() != 0 &&                              // not empty
-                  (i <= iBackNormalize() ||
-                   i >= iFrontNormalize()))                   // in range
-            cerr << data[i];
-      }
-      cerr << " }";
-   }
-   cerr << "\n";
-
-   // display the front and back with the normalized values in ()s
-   if (numCapacity)
-   {
-      cerr << "\tiFront = " << iFront
-           << " ("          << iFrontNormalize() << ")\n";
-      cerr << "\tiBack  = " << iBack
-           << " ("          << iBackNormalize()  << ")\n";
-   }
-}
-#else
-template <class T>
-void deque <T> :: display() const
-{
-}
-#endif // DEBUG
