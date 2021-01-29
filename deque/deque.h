76b1d3faa9cb87a96ee07da2c14f64e775008200
Dallin Gilbert <djgilb24@byui.edu>
2021-01-28 17:26:17 -0700

    adding complete files for deque

--- deque/deque.h
+++ deque/deque.h
@@ -13,8 +13,8 @@
  *    Br. Helfrich
  ************************************************************************/
 
-#ifndef DEQUEUE_H
-#define DEQUEUE_H
+#ifndef DEQUE_H
+#define DEQUE_H
 #include <iostream>
 #include <cstddef>
 #include <cassert>  // because I am paranoid
@@ -57,13 +57,14 @@ namespace custom
         int iFrontNormalized() const;
         int iBackNormalized() const;
         int iNormalized(int index) const;
+        void display() const;
 
     public:
         // default constructor and non-default constructors
         deque()
         {
             setCapacity(0);
-            setBack(0);
+            setBack(-1);
             setFront(0);
             this->data = NULL;
         }
@@ -83,7 +84,7 @@ namespace custom
         deque& operator = (const deque<T>& rhs) throw(const char*);
 
         // getters
-        int size() const { return (getBack() - (getFront() + 1)); }
+        int size() const { return getBack() - getFront() + 1; }
         int getBack() const { return iBack; }
         int getFront() const { return iFront; }
 
@@ -392,4 +393,90 @@ namespace custom
             return data[iBackNormalized()];
     }
 };
-#endif // DEQUEUE_H
+#endif // DEQUE_H
+
+// this code should be copy-pasted into deque.h immediately after
+// the class definition. Also, put the following inside the class
+// definition itself:
+//     void display() const;
+// Please remove this comment when you have done that.
+//
+// To "turn this on", you need to add -DDEBUG to the makefile
+// in all the g++ statements. Please make sure you remove
+// these when you submit your assignment.
+//
+// Finally, to display the contents of the deque at a givem
+// point in the program (hopefully where the bugs are), then
+// you should add:
+//     d.display();
+
+#ifdef DEBUG
+
+using std::cerr;
+/****************************************************
+ * DISPLAY DEQUE
+ * display the contents of a deque for debug purposes
+ ****************************************************/
+template <class T>
+void deque <T> :: display() const
+{
+   // display the header info
+   cerr << "\ndeque<T>::display()\n";
+   cerr << "\tnumCapacity = " << numCapacity << "\n";
+
+   // display the contents of the array
+   cerr << "\tdata = ";
+   if (numCapacity == 0)
+      cerr << "NULL";
+   else
+   {
+      cerr << "{ ";
+      for (int i = 0; i < numCapacity; i++)
+      {
+         if (i != 0)
+            cerr << ", ";
+
+         // not wrapped
+         //      0   1   2   3   4   5   6
+         //    +---+---+---+---+---+---+---+
+         //    |   |   | A | B | C |   |   |
+         //    +---+---+---+---+---+---+---+
+         // iFront = 9     iFrontNormalize() = 2
+         // iBack  = 11    iBackNormalize()  = 4
+         if (iFrontNormalize() <= iBackNormalize() &&  // not wrapped
+             iFrontNormalize() <= i &&
+             i <= iBackNormalize())                        // in range
+            cerr << data[i];
+
+         // wrapped
+         //      0   1   2   3   4   5   6
+         //    +---+---+---+---+---+---+---+
+         //    | B | C |   |   |   |   | A |
+         //    +---+---+---+---+---+---+---+
+         // iFront = -8    iFrontNormalize() = 6
+         // iBack  = -6    iBackNormalize()  = 1
+         else if (iFrontNormalize() > iBackNormalize() && // wrapped
+                  size() != 0 &&                              // not empty
+                  (i <= iBackNormalize() ||
+                   i >= iFrontNormalize()))                   // in range
+            cerr << data[i];
+      }
+      cerr << " }";
+   }
+   cerr << "\n";
+
+   // display the front and back with the normalized values in ()s
+   if (numCapacity)
+   {
+      cerr << "\tiFront = " << iFront
+           << " ("          << iFrontNormalize() << ")\n";
+      cerr << "\tiBack  = " << iBack
+           << " ("          << iBackNormalize()  << ")\n";
+   }
+}
+#else
+template <class T>
+void deque <T> :: display() const
+{
+}
+#endif // DEBUG
---
+++ deque/nowServing.h
@@ -0,0 +1,21 @@
+/***********************************************************************
+ * Header:
+ *    NOW SERVING
+ * Summary:
+ *    This will contain just the prototype for nowServing(). You may
+ *    want to put other class definitions here as well.
+ * Author
+ *    Austin Earl
+ *    Jeffrey Chumley
+ ************************************************************************/
+
+#ifndef NOW_SERVING_H
+#define NOW_SERVING_H
+
+#include "deque.h"     // for DEQUE
+
+// the interactive nowServing program
+void nowServing();
+
+#endif // NOW_SERVING_H
+
