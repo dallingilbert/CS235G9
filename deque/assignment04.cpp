9ceccd23a81dfc7aac9c685afa1bd3b1a54fcb23
Dallin Gilbert <djgilb24@byui.edu>
2021-01-28 17:47:27 -0700

    opening test 2

--- deque/assignment04.cpp
+++ deque/assignment04.cpp
@@ -36,7 +36,7 @@ void testErrors();
 // I suggest first commenting out all of these tests, then try to use only
 // TEST1.  Then, when TEST1 works, try TEST2 and so on.
 #define TEST1   // for testSimple()
-//#define TEST2   // for testPushPopFront()
+#define TEST2   // for testPushPopFront()
 //#define TEST3   // for testWrapping()
 //#define TEST4   // for testErrors()
 
